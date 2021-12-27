582a3317b79073aa146298b87266a443851b4d0c
Alok Kumar <Alokkumar8@users.noreply.github.com>
2021-12-27 16:04:43 +0530

    Update Weather.kt

--- app/src/main/java/com/example/demoweather/model/Weather.kt
+++ app/src/main/java/com/example/demoweather/model/Weather.kt
@@ -2,9 +2,5 @@ package com.example.demoweather.model
 
 data class Weather(
     var id: String = "",
-    var temperature: String = "",
-    var body: String = "",
-    var timestamp: Long = 0L,
-    var language: String = "",
-    var user_id: String = ""
+    var temperature: String = ""
 )
